80513a25d2148cdd7c2eb67696d28a48a0c25ce6
Toma Nistor <toma.nistor@gmail.com>
2018-03-19 22:27:02 -0700

    Created modal

---
+++ modal/script.js
@@ -0,0 +1,26 @@
+Vue.component ('modal', {
+  template: `
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box">
+          <slot></slot>
+        </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="$emit('close')">X</button>
+    </div>
+  `,
+
+  data() {
+    return {
+
+    };
+  }
+})
+
+new Vue({
+  el: '#app',
+  data: {
+    showModal: false
+  }
+})
