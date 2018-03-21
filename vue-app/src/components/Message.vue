7cbfc9e3b81a7d478d7d36a9d96e4b286586d81e
Toma Nistor <toma.nistor@gmail.com>
2018-03-21 15:30:58 -0700

    Connected simple working vue component

---
+++ vue-app/src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box info">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="scss">
+  .box {
+    background: #e3e3e3;
+    padding: 10px;
+    border: 1px solid #000;
+    margin-bottom: 5px;
+  }
+</style>
